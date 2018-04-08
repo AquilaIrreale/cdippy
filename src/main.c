5fc8a5bd22a3a9c8000ee3135a78e70148848b0f
Simone Cimarelli <aquilairreale@ymail.com>
2018-04-09 00:08:04 +0200

    Make cdippy output info for every dislodged army
    Which armies have been dislodged and where they can retreat to

--- src/main.c
+++ src/main.c
@@ -22,6 +22,33 @@ void execute()
 
     putchar('\n');
 
+    enum territory t1;
+    for (t1 = 0; t1 < TERR_N; t1++) {
+        if (dislodged(t1)) {
+            printf("%llu:", (long long unsigned)t1);
+
+            const enum territory *neighs = territories[t1].unit == ARMY
+                                         ? territories[t1].land_neighs
+                                         : territories[t1].sea_neighs;
+
+            size_t neighs_n = territories[t1].unit == ARMY
+                            ? territories[t1].land_neighs_n
+                            : territories[t1].sea_neighs_n;
+
+            for (i = 0; i < neighs_n; i++) {
+                enum territory t2 = neighs[i];
+
+                if (can_retreat(t1, t2)) {
+                    printf(" %llu", (long long unsigned)t2);
+                }
+            }
+
+            putchar('\n');
+        }
+    }
+
+    putchar('\n');
+
     orders_n = 0;
 }
 
